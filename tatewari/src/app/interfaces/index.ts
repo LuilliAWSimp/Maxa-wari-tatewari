3e1245dc33782fcbd9d588977fa3bb591f67edb2
LuilliAWSimp <luilliielsexy@gmail.com>
2024-02-13 21:25:19 -0600

    Tabs
    Error en tabs corregido, pero sigue sin funcionar de todo

---
+++ tatewari/src/app/interfaces/index.ts
@@ -0,0 +1,7 @@
+
+export interface TopLevel {
+  id_user?: number;
+  name?: string;
+  email?: string;
+  password?: string;
+}
---
+++ tatewari/src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+//import { TopLevel } from '../../interfaces';
+import { TopLevel } from '../../interfaces/index';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page implements OnInit {
+  public resp: TopLevel[] = [];
+
+  constructor(private newService: ApiService) {}
+  ngOnInit() {
+    this.newService.getTopHeadlines().subscribe((resp) => {
+      console.log(resp); // Imprime el objeto TopLevel o arreglo TopLevel en la consola
+      if (Array.isArray(resp)) {
+        this.resp = resp; // Si es un arreglo, asigna directamente
+      } else {
+        this.resp = [resp]; // Si es un objeto, envuélvelo en un arreglo antes de asignar
+      }
+    });
+  }
+  eliminarDato(id_user: number) {
+    this.newService.eliminarDato(id_user).subscribe(
+      () => {
+        console.log('Usuario eliminado con exito');
+        // Filtrar la lista actual para quitar el elemento eliminado
+        this.resp = this.resp.filter(item => item.id_user != id_user);
+      },
+      (error) => {
+        console.error('Error al eliminar el producto:', error);
+      }
+    );
+  }
+}
