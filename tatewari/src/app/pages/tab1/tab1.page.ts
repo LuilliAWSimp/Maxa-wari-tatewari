439da8af6d66a188e87ced381fcf1ad3b239eb6f
LuilliAWSimp <luilliielsexy@gmail.com>
2024-02-14 12:44:06 -0600

    Src
    Instalación de dependencias faltantes en la carpeta Src

--- tatewari/src/app/pages/tab1/tab1.page.ts
+++ tatewari/src/app/pages/tab1/tab1.page.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+<<<<<<< Updated upstream
 //import { TopLevel } from '../../interfaces';
+=======
+>>>>>>> Stashed changes
 import { TopLevel } from '../../interfaces/index';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
+<<<<<<< Updated upstream
   styleUrls: ['tab1.page.scss'],
 })
+=======
+  styleUrls: ['tab1.page.scss']
+})
+
+>>>>>>> Stashed changes
 export class Tab1Page implements OnInit {
   public resp: TopLevel[] = [];
 
   constructor(private newService: ApiService) {}
+<<<<<<< Updated upstream
   ngOnInit() {
     this.newService.getTopHeadlines().subscribe((resp) => {
       console.log(resp); // Imprime el objeto TopLevel o arreglo TopLevel en la consola
@@ -35,3 +45,31 @@ export class Tab1Page implements OnInit {
     );
   }
 }
+=======
+
+  ngOnInit() {
+    this.newService.getTopHeadlines()
+      .subscribe(resp => {
+        console.log(resp);
+        if (Array.isArray(resp)) {
+          this.resp = resp; 
+        } else {
+          this.resp = [resp]; 
+        }
+      });
+  }
+
+  eliminarDato(id: number) {
+    this.newService.eliminarDato(id).subscribe(
+      () => {
+        console.log("Producto eliminado con éxito");
+        // Filtrar la lista actual para quitar el elemento eliminado
+        this.resp = this.resp.filter(item => item.id !== id);
+      },
+      error => {
+        console.error("Error al eliminar el producto:", error);
+      }
+    );
+  }
+}
+>>>>>>> Stashed changes
--- tatewari/src/app/services/api.service.ts
+++ tatewari/src/app/services/api.service.ts
@@ -1,9 +1,15 @@
+<<<<<<< Updated upstream
 import { Injectable, Pipe } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+=======
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+>>>>>>> Stashed changes
 import { TopLevel } from '../interfaces';
 import { Observable, pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+<<<<<<< Updated upstream
 @Injectable({
   providedIn: 'root',
 })
@@ -22,4 +28,31 @@ export class ApiService {
   eliminarDato(id_user: number): Observable<{}> {
     return this.http.delete<any>(`${this.apiUrl}?id=${id_user}`);
   }
+=======
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  public apiUrl = 'http://127.0.0.1:80/api1/method.php'; // Reemplaza con la URL de tu API
+
+  constructor(private http: HttpClient) {}
+
+  // Método para obtener los datos
+  getTopHeadlines(): Observable<TopLevel> {
+    return this.http.get<TopLevel>('http://127.0.0.1:80/api1/method.php').pipe(
+      map(resp => resp)
+    );
+  }
+
+  // Método para enviar datos por POST
+  postDatos(datos: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, datos, { responseType: 'text' as 'json' });
+  }
+
+  eliminarDato(id: number): Observable<string> {
+  return this.http.delete<string>(`${this.apiUrl}?id=${id}`, { responseType: 'text' as 'json' });
+}
+
+>>>>>>> Stashed changes
 }
